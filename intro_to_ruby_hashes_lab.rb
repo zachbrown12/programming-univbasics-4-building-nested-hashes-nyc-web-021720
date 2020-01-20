c11b741f0a394a2830c441a66a64950051f829e6
Zach Brown <zachmb22@gmail.com>
2020-01-20 23:34:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -85,7 +85,7 @@ def monopoly_with_fourth_tier
       {
         "mortgage_value"=>"$400"
       },
-      :shortline=>
+      :shortline_railroad=>
       {
         "mortgage_value"=>"$800"
       }
