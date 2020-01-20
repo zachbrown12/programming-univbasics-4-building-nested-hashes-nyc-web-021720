868849a1a3002775603cc5bd1b19049ce990dc0a
Zach Brown <zachmb22@gmail.com>
2020-01-20 23:31:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,12 @@ def monopoly_with_third_tier
   monopoly =  {
   railroads:{
     pieces:4,
-    rent_in_dollars:{},
+    rent_in_dollars:{
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
     names:{}
   }
 }
